4d33b163b57b461d6633a6e38ad7729e4828f4f4
Vinicius Galassi <vinicius.galassi@v4company.com>
2026-05-04 13:55:35 -0300

    exercicio 6

--- src/exercicios/ExercicioSeis.tsx
+++ src/exercicios/ExercicioSeis.tsx
@@ -1,12 +1,48 @@
-﻿import ExercicioPlaceholder from "../components/ExercicioPlaceholder";
+﻿import { useEffect, useState } from "react";
+import ExercicioPlaceholder from "../components/ExercicioPlaceholder";
 
 function ExercicioSeis() {
+  const [time, setTime] = useState<Date>(new Date())
+  const [visibility, setVisibility] = useState<boolean>(true)
+  const [format, setFormat] = useState<boolean>(true)
+  function handleButtonClick(setter: React.Dispatch<React.SetStateAction<boolean>>) {
+    setter((prop) => !prop)
+  }
+
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date())
+    }, 1000);
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
   return (
-    <ExercicioPlaceholder
-      titulo="Exercicio 06 - Relogio com useEffect"
-      arquivoMarkdown="06-relogio-com-useeffect.md"
-    />
+    <>
+      <ExercicioPlaceholder
+        titulo="Exercicio 06 - Relogio com useEffect"
+        arquivoMarkdown="06-relogio-com-useeffect.md"
+      />
+      {visibility && (
+          <h1 className="text-2xl font-bold">
+            {format 
+              ? time.toLocaleTimeString('pt-BR', { hour12: false }) 
+              : time.toLocaleTimeString('pt-BR', { hour12: true })}
+          </h1>
+        )}
+      <button onClick={() => handleButtonClick(setVisibility)} className="bg-white border-1 text-black m-2 cursor-pointer rounded-sm p-1">
+        {visibility ? "Ocultar" : "Mostrar"}
+      </button>
+      
+      {visibility && <button onClick={() => handleButtonClick(setFormat)} className="bg-white border-1 text-black m-2 cursor-pointer rounded-sm p-1">
+        {format ? "Trocar para 12h" : "Trocar para 24h"}
+      </button>}
+      
+
+    </>
   );
+
 }
 
 export default ExercicioSeis;
